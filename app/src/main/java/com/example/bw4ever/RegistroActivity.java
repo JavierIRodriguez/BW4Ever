78a4fe53af1f299b09b409864e22b8cff0d02cc8
JavierIRodriguez <JavierRodriguez39@inacapmail.cl>
2020-06-24 18:13:06 -0400

    Se agregan verificaciones adicionales a los campos de ingreso de información del Registro y algunos cambios en el diseño.

--- app/src/main/java/com/example/bw4ever/RegistroActivity.java
+++ app/src/main/java/com/example/bw4ever/RegistroActivity.java
@@ -19,9 +19,12 @@ import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegistroActivity extends AppCompatActivity {
 
-    EditText txtNombre, txtCorreo, txtPassword;
+    EditText txtNombre, txtCorreo, txtPassword, txtConf;
     FirebaseAuth firebaseAuth;
     FirebaseDatabase firebaseDatabase;
 
@@ -35,6 +38,7 @@ public class RegistroActivity extends AppCompatActivity {
         txtNombre = findViewById(R.id.txt_registroNombre);
         txtCorreo = findViewById(R.id.txt_registroMail);
         txtPassword = findViewById(R.id.txt_registroPassword);
+        txtConf = findViewById(R.id.txt_registroConfirmarPass);
 
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseDatabase = FirebaseDatabase.getInstance();
@@ -44,45 +48,62 @@ public class RegistroActivity extends AppCompatActivity {
         progressDialog.setCancelable(false);
     }
 
+    public boolean checkEmail (String _correo){
+        Pattern pattern = Pattern.compile("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}");
+        Matcher mat = pattern.matcher(_correo);
+        if(mat.matches()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
     public void registrarUsuario(View view) {
         final String nombre = txtNombre.getText().toString();
         final String correo = txtCorreo.getText().toString();
         final String password = txtPassword.getText().toString();
+        final String confPass = txtConf.getText().toString();
 
-        if(correo.isEmpty() || nombre.isEmpty() || password.isEmpty() || password.length() < 6){
-            Toast.makeText(this, "Verifique datos!", Toast.LENGTH_SHORT).show();
+        if(!checkEmail(correo)){
+            Toast.makeText(this, "Debe ingresar un correo válido!", Toast.LENGTH_SHORT).show();
         }
-        else{
-            progressDialog.setMessage("Conectado con la Base de Datos...");
-            progressDialog.show();
+        else {
+            if (!password.equals(confPass)) {
+                Toast.makeText(this, "Las contraseñas deben coincidir!", Toast.LENGTH_SHORT).show();
+            } else {
+                if (correo.isEmpty() || nombre.isEmpty() || password.isEmpty() || password.length() < 6) {
+                    Toast.makeText(this, "Verifique datos!", Toast.LENGTH_SHORT).show();
+                } else {
+                    progressDialog.setMessage("Conectando con la Base de Datos...");
+                    progressDialog.show();
 
-            firebaseAuth.createUserWithEmailAndPassword(correo, password)
-                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if(task.isSuccessful()){
-                                Usuario user = new Usuario();
-                                user.setNombre(nombre);
-                                user.setCorreo(correo);
-                                user.setPassword(password);
+                    firebaseAuth.createUserWithEmailAndPassword(correo, password)
+                            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        Usuario user = new Usuario();
+                                        user.setNombre(nombre);
+                                        user.setCorreo(correo);
+                                        user.setPassword(password);
 
-                                DatabaseReference ref = firebaseDatabase.getReference("Usuarios");
-                                ref.push().setValue(user);
+                                        DatabaseReference ref = firebaseDatabase.getReference("Usuarios");
+                                        ref.push().setValue(user);
 
-                                Toast.makeText(RegistroActivity.this, "Usuario registrado con exito!", Toast.LENGTH_SHORT).show();
-                            }
-                            else{
-                                if(task.getException() instanceof FirebaseAuthUserCollisionException){
-                                    Toast.makeText(RegistroActivity.this, "Este correo ya se encuentra registrado!", Toast.LENGTH_SHORT).show();
+                                        Toast.makeText(RegistroActivity.this, "Usuario registrado con exito!", Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        if (task.getException() instanceof FirebaseAuthUserCollisionException) {
+                                            Toast.makeText(RegistroActivity.this, "Este correo ya se encuentra registrado!", Toast.LENGTH_SHORT).show();
+                                        } else {
+                                            Toast.makeText(RegistroActivity.this, "Ocurrió un error!", Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                    progressDialog.dismiss();
                                 }
-                                else{
-                                    Toast.makeText(RegistroActivity.this, "Ocurrió un error!", Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                            progressDialog.dismiss();
-                        }
-                    });
+                            });
+                }
+            }
         }
-
     }
 }
